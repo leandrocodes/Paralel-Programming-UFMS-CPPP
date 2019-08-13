807020087473fc78431d34bca782db56210e5d38
Leandro <leaxviana140@gmail.com>
2019-08-13 13:47:16 -0400

    Initial commit

---
+++ helloworld.cpp
@@ -0,0 +1,20 @@
+#include "mpi.h"
+
+int main(int argc, char** argv){
+    
+    MPI_Init(NULL, NULL);
+    
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    int world_rank;
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    printf("Hello Wolrd from rank %d"
+            " out of %d processors\n",
+            world_rank, world_size);
+
+
+    MPI_Finalize();
+}
